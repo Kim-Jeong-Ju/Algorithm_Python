ac494e86af1cad2c9f4639762346d2eb4129cbee
Kim-Jeong-Ju <arixter1274@naver.com>
2024-07-21 01:18:21 +0900

    0713_알고리즘_Programmers풀이_68

---
+++ 102_pair_of_prime_number.py
@@ -0,0 +1,25 @@
+## 문제 097. Addition Cycle --- 백준 No.1110
+
+cycle = 0
+
+old_num = input()
+new_num = '-1'
+
+while int(old_num) != int(new_num):
+    if new_num == '-1':
+        if len(old_num) == 1:
+            new_num = old_num + old_num
+        else:
+            if len(str(int(old_num[0]) + int(old_num[1]))) == 1:
+                new_num = old_num[1] + str(int(old_num[0]) + int(old_num[1]))[0]
+            else:
+                new_num = old_num[1] + str(int(old_num[0]) + int(old_num[1]))[1]
+    else:
+        if len(str(int(new_num[0]) + int(new_num[1]))) == 1:
+            new_num = new_num[1] + str(int(new_num[0]) + int(new_num[1]))[0]
+        else:
+            new_num = new_num[1] + str(int(new_num[0]) + int(new_num[1]))[1]
+
+    cycle += 1
+
+print(cycle)
