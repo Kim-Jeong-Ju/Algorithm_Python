400990fbe8120fcf26ad01616df5a8cbb6fd70f3
Kim-Jeong-Ju <arixter1274@naver.com>
2025-07-20 23:52:25 +0900

    0304_알고리즘_Programmers풀이_70

---
+++ 104_statistics.py
@@ -0,0 +1,29 @@
+## 문제 104. 약수 --- 백준 No.1037
+
+import sys
+input = sys.stdin.readline
+
+Array = []
+N = int(input())
+for num in range(N):
+    Array.append(int(input()))
+
+def alpha_count(array):
+    alpha, beta = dict(), []
+    for Anum in array:
+        if Anum in alpha.keys(): alpha[Anum] += 1
+        else: alpha[Anum] = 1
+
+    max_value = max(alpha.values())
+    for Bnum in alpha:
+        if max_value == alpha[Bnum]: beta.append(Bnum)
+
+    if len(beta) > 1 : return beta[1]
+    else: return beta[0]
+
+Array.sort()
+
+print(round(sum(Array) / N))
+print(Array[N // 2])
+print(alpha_count(Array))
+print(Array[-1] - Array[0])
