959bc02deb94541e5761dea94a152243e18c93ab
Kim-Jeong-Ju <arixter1274@naver.com>
2025-10-19 01:43:09 +0900

    1019_알고리즘_BOJ풀이_15

---
+++ 107_alteration_base.py
@@ -0,0 +1,18 @@
+## 문제 107. 진법 변환 --- 백준 No.2745
+
+alpha_crit = {'A': 10, 'B': 11, "C": 12, 'D': 13, 'E': 14, 'F': 15,
+              'G': 16, 'H': 17, 'I': 18, 'J': 19, 'K': 20, 'L': 21, 'M': 22,
+              'N': 23, 'O': 24, 'P': 25, 'Q': 26, 'R': 27, 'S': 28,
+              'T': 29, 'U': 30, 'V': 31, 'W': 32, 'X': 33, 'Y': 34, 'Z': 35}
+
+A_num, B_num = input().split()
+A_num = A_num[::-1]
+B_num = int(B_num)
+
+answer = 0
+
+for idx in range(len(A_num)):
+    if A_num[idx].isalpha(): answer += alpha_crit[A_num[idx]] * (B_num ** idx)
+    else: answer += int(A_num[idx]) * (B_num ** idx)
+
+print(answer)
---
+++ 108_laundary_ceo.py
@@ -0,0 +1,17 @@
+## 문제 108. 세탁소 사장 동혁 --- 백준 No.2720
+
+T = int(input())
+
+for _ in range(T):
+    Array = [25, 10, 5, 1]
+    Brray = [0, 0, 0, 0]
+
+    amount = int(input())
+
+    for idx in range(4):
+        div, mod = divmod(amount, Array[idx])
+
+        amount -= Array[idx] * int(div)
+        Brray[idx] += int(div)
+
+    print(f"{Brray[0]} {Brray[1]} {Brray[2]} {Brray[3]}")
